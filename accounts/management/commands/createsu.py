791dfff3d39888dc405ab18ee8a953a29521c772
Fahd Mannaa <egypingblog@gmail.com>
2022-05-13 19:25:51 +0300

    collect statics

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -1,15 +1,9 @@
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
-    help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                password='Django@123',
-            )
-        print('Superuser has been created.')
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin3", "admin@admin.com", "Django@123")
